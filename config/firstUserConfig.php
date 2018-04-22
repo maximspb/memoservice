772fdda0209a6a0378d58723a06f8eb4b8d9b863
maximspb <maxim.kuzn@gmail.com>
2018-04-22 23:15:16 +0300

    скорректирована миграция User. Добавлено создание первого пользователя в системе

--- config/firstUserConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'email' => 'adm@test242342332523.com',
-    'password' => 'adminpassword',
-
-];
--- config/mailConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'host' => 'smtp.mail.ru',
-    'username' => '',
-    'password' => '',
-];
